5826572ea9bff67ef82f4a91cf10544932dedff1
ScorpSuki <scorp.suki@mail.ru>
2026-04-27 13:41:23 +0500

    Change add function to compute cubic difference
    Modify add function to return a cubic difference.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def add(a, b):
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise ValueError("Тек сан болуы қажет")
-    return a + b
+    return ((a + b)**3)-((a+b)**2)
 
 if __name__ == "__main__":
     print("Нәтиже:", add(2, 3))
